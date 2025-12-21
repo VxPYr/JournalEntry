a0b8db2a4e580773113a4c3cf7c5923edaf300b5
VxPYr <vishnujaswal2004@gmail.com>
2025-12-21 21:28:05 +0530

    Added UserServiceTest

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTest.java
@@ -0,0 +1,22 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUsername() {
+        assertNotNull(userRepository.findByUsername("bomb"));
+    }
+}
